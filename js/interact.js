e78d0372cb26b6aac0cbec01a7d43ca18989ad12
ale <asmuginatophat@gmail.com>
2024-06-19 13:40:34 -0400

    sfx mainly and also self insert trolo

---
+++ js/interact.js
@@ -0,0 +1,7 @@
+window.addEventListener("click", function(){
+    document.querySelector("#click").play()
+});
+
+document.querySelector("#startbtn").onclick = function(){
+    document.querySelector("#ding").play()
+}
